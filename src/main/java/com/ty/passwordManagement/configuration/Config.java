83d17d880cd53fe7ffbd75fe66d6da7a06e46b47
manoj kulli <sarupunit@gmail.com>
2022-11-25 14:38:06 +0530

    userDao updated

--- src/main/java/com/ty/passwordManagement/configuration/Config.java
+++ src/main/java/com/ty/passwordManagement/configuration/Config.java
@@ -1,7 +1,13 @@
 package com.ty.passwordManagement.configuration;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
@@ -9,4 +15,9 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 public class Config {
 
+	@Bean(name="em")
+	@Scope(value="prototype")
+	public EntityManager getEmf(){	
+		return Persistence.createEntityManagerFactory("project").createEntityManager();
+	}
 }
--- src/main/java/com/ty/passwordManagement/configuration/ConfigurationWebInitializer.java
+++ src/main/java/com/ty/passwordManagement/configuration/ConfigurationWebInitializer.java
@@ -1,5 +1,25 @@
 package com.ty.passwordManagement.configuration;
 
-public class ConfigurationWebInitializer {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ConfigurationWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class [] {Config.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String [] {"/"};
+	}
 
 }
